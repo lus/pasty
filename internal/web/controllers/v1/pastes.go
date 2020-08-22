3aba9adb32aa440b5526071200995de1f7d9e60a
Lukas SP <kbrt@protonmail.com>
2020-08-23 00:40:20 +0200

    Implement paste creation endpoint

--- internal/web/controllers/v1/pastes.go
+++ internal/web/controllers/v1/pastes.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"encoding/json"
+	"github.com/Lukaesebrot/pasty/internal/pastes"
 	"github.com/Lukaesebrot/pasty/internal/storage"
 	"github.com/bwmarrin/snowflake"
 	"github.com/fasthttp/router"
@@ -33,6 +34,42 @@ func v1GetPaste(ctx *fasthttp.RequestCtx) {
 	if paste == nil {
 		ctx.SetStatusCode(fasthttp.StatusNotFound)
 		ctx.SetBodyString("paste not found")
+		return
+	}
+
+	// Respond with the paste
+	jsonData, err := json.Marshal(paste)
+	if err != nil {
+		ctx.SetStatusCode(fasthttp.StatusInternalServerError)
+		ctx.SetBodyString(err.Error())
+		return
+	}
+	ctx.SetBody(jsonData)
+}
+
+// v1PostPaste handles the 'POST /v1/pastes' endpoint
+func v1PostPaste(ctx *fasthttp.RequestCtx) {
+	// Unmarshal the body
+	values := make(map[string]string)
+	err := json.Unmarshal(ctx.PostBody(), &values)
+	if err != nil {
+		ctx.SetStatusCode(fasthttp.StatusBadRequest)
+		ctx.SetBodyString("invalid request body")
+		return
+	}
+
+	// Validate the content of the paste
+	if values["content"] == "" {
+		ctx.SetStatusCode(fasthttp.StatusBadRequest)
+		ctx.SetBodyString("missing 'content' field")
+		return
+	}
+
+	// Create the paste object
+	paste, err := pastes.Create(values["content"])
+	if err != nil {
+		ctx.SetStatusCode(fasthttp.StatusInternalServerError)
+		ctx.SetBodyString(err.Error())
 		return
 	}
 
