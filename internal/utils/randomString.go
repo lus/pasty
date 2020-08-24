7d0c25fb2f54dea5de2f30b781c4f41aa0689816
Lukas SP <kbrt@protonmail.com>
2020-08-24 20:28:09 +0200

    Seed random generator

--- internal/utils/randomString.go
+++ internal/utils/randomString.go
@@ -1,12 +1,16 @@
 package utils
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 // stringContents holds the chars a random string can contain
 const stringContents = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 // RandomString returns a random string with the given length
 func RandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
 	bytes := make([]byte, length)
 	for i := range bytes {
 		bytes[i] = stringContents[rand.Int63()%int64(len(stringContents))]
