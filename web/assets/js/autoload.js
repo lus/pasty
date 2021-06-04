42a400ac19f64cbac26502317a846dd864991217
Lukas Schulte Pelkum <kbrt@protonmail.com>
2021-06-04 12:36:03 +0200

    Revert line highlighting

--- web/assets/js/autoload.js
+++ web/assets/js/autoload.js
@@ -29,7 +29,7 @@ async function loadPaste() {
         const split = location.pathname.replace("/", "").split(".");
         const pasteID = split[0];
         const language = split[1];
-    
+
         // Retrieve the paste from the API and redirect the user to the main page if it could not be found
         const response = await api.getPaste(pasteID);
         if (!response.ok) {
@@ -37,63 +37,29 @@ async function loadPaste() {
             return;
         }
         const data = await response.json();
-    
+
         // Adjust the button states
         document.getElementById("btn_save").setAttribute("disabled", true);
         document.getElementById("btn_delete").removeAttribute("disabled");
         document.getElementById("btn_copy").removeAttribute("disabled");
-    
+
         // Set the paste content to the DOM
         const code = document.getElementById("code");
         code.innerHTML = language
             ? hljs.highlight(language, data.content).value
             : hljs.highlightAuto(data.content).value;
-        
+
         // Display the line numbers
         const lineNOs = document.getElementById("linenos");
         lineNOs.innerHTML = data.content.split(/\n/).map((_, index) => `<span>${index + 1}</span>`).join('');
 
-        const sharedLine = parseInt(window.location.hash.toLowerCase().replace("#l", ""), 10);
-        if (sharedLine) {
-            lineNOs.innerHTML = data.content.split(/\n/).map((_, index) => {
-                return index + 1 === sharedLine
-                    ? `<span class="highlight">${index + 1}</span>`
-                    : `<span>${index + 1}</span>`;
-            }).join('');
-
-            let html = code.innerHTML.split(/\n/);
-            html[sharedLine-1] = `<span class="highlight">${html[sharedLine-1]}</span>`;
-            code.innerHTML = html.join("\n");
-        }
-
-        // TODO: Update this shitty construct
-        lineNOs.childNodes.forEach(node => {
-            node.addEventListener("click", function(_) {
-                const address = location.protocol + "//" + location.host + location.pathname + "#L" + node.innerText;
-                location.replace(address);
-                location.reload();
-            });
-            node.addEventListener("mouseover", function(_) {
-                let html = code.innerHTML.split(/\n/);
-                const index = parseInt(node.innerText, 10) - 1;
-                html[index] = `<span class="highlight">${html[index]}</span>`;
-                code.innerHTML = html.join("\n");
-            });
-            node.addEventListener("mouseout", function(_) {
-                let html = code.innerHTML.split(/\n/);
-                const index = parseInt(node.innerText, 10) - 1;
-                html[index] = html[index].substring(24, html[index].length - 7);
-                code.innerHTML = html.join("\n");
-            });
-        });
-        
         // Set the PASTE_ID variable
         PASTE_ID = pasteID;
     } else {
         const input = document.getElementById("input");
         input.classList.remove("hidden");
         input.focus();
-        window.addEventListener("keydown", function(event) {
+        window.addEventListener("keydown", function (event) {
             if (event.keyCode != 9) return;
             event.preventDefault();
             input.value += "    ";
