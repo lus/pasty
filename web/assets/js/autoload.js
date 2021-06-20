a7978558c83e74ac4558df7310299c6637317888
Lukas Schulte Pelkum <kbrt@protonmail.com>
2021-06-20 15:44:54 +0200

    Calculate line number offset based on line overflow

--- web/assets/js/autoload.js
+++ web/assets/js/autoload.js
@@ -8,6 +8,12 @@ import * as notifications from "./notifications.js";
 buttons.setupButtons();
 buttons.setupKeybinds();
 
+// Define element handles
+const versionElement = document.getElementById("version");
+const lineNOsElement = document.getElementById("linenos");
+const codeElement = document.getElementById("code");
+const inputElement = document.getElementById("input");
+
 // Load the API information
 async function loadAPIInformation() {
     const response = await api.getAPIInformation();
@@ -17,12 +23,13 @@ async function loadAPIInformation() {
         return;
     }
     const data = await response.json();
-    document.getElementById("version").innerText = data.version;
+    versionElement.innerText = data.version;
 }
 loadAPIInformation();
 
 // Try to load a paste if one exists
 export let PASTE_ID;
+let CODE;
 async function loadPaste() {
     if (location.pathname !== "/") {
         // Define the paste ID and language
@@ -36,7 +43,7 @@ async function loadPaste() {
             location.replace(location.protocol + "//" + location.host);
             return;
         }
-        const data = await response.json();
+        CODE = (await response.json()).content;
 
         // Adjust the button states
         document.getElementById("btn_save").setAttribute("disabled", true);
@@ -44,30 +51,41 @@ async function loadPaste() {
         document.getElementById("btn_copy").removeAttribute("disabled");
 
         // Set the paste content to the DOM
-        const code = document.getElementById("code");
-        code.innerHTML = language
-            ? hljs.highlight(language, data.content).value
-            : hljs.highlightAuto(data.content).value;
+        codeElement.innerHTML = language
+            ? hljs.highlight(language, CODE).value
+            : hljs.highlightAuto(CODE).value;
 
         // Display the line numbers
-        const lineNOs = document.getElementById("linenos");
-        lineNOs.innerHTML = data.content.split(/\n/).map((_, index) => `<span>${index + 1}</span>`).join('');
+        renderLineNumbers();
+        window.addEventListener("resize", renderLineNumbers);
 
         // Set the PASTE_ID variable
         PASTE_ID = pasteID;
     } else {
-        const input = document.getElementById("input");
-        input.classList.remove("hidden");
-        input.focus();
+        inputElement.classList.remove("hidden");
+        inputElement.focus();
         window.addEventListener("keydown", function (event) {
             if (event.keyCode != 9) return;
             event.preventDefault();
 
-            insertTextAtCursor(input, "    ");
+            insertTextAtCursor(inputElement, "    ");
         });
     }
 }
 spinner.surround(loadPaste);
+
+function renderLineNumbers() {
+    lineNOsElement.innerHTML = CODE.split(/\n/).map((line, index) => {
+        let lineWidth = getTextWidth(line, "16px Source Code Pro");
+        let linesSpace = Math.ceil(lineWidth / codeElement.offsetWidth);
+
+        let result = `<span>${index+1}</span>`;
+        if (linesSpace > 1) {
+            result += "<span></span>".repeat(linesSpace - 1);
+        }
+        return result;
+    }).join("");
+}
 
 // 1:1 skid from https://stackoverflow.com/questions/7404366/how-do-i-insert-some-text-where-the-cursor-is
 function insertTextAtCursor(el, text) {
@@ -84,4 +102,13 @@ function insertTextAtCursor(el, text) {
         range.text = text;
         range.select();
     }
+}
+
+// Also a kind of skid
+function getTextWidth(text, font) {
+    let canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+    let context = canvas.getContext("2d");
+    context.font = font;
+    let metrics = context.measureText(text);
+    return metrics.width;
 }
